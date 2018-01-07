d3627346d9bda7545f4daee7bf36b24b007d101c
josuesf <113552@unsaac.edu.pe>
2018-01-06 23:54:47 -0500

    Creacion de codigo qr y comentarios

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ var aws = require('aws-sdk')
 var multerS3 = require('multer-s3')
 
 var ObjectID = mongojs.ObjectID; //mongodb://<dbuser>:<dbpassword>@ds157653.mlab.com:57653/gn8db
-var db = mongojs(process.env.MONGO_URL || 'mongodb://gn8user:#goodNight@ds157653.mlab.com:57653/gn8db');
+var db = mongojs(process.env.MONGO_URL);
 var app = express();
 var server = http.createServer(app);
 var websocket = socketio(server);
@@ -49,7 +49,7 @@ app.post('/ws/create_post', upload.single('picture'), function (req, res, next) 
   //   || !inputs.codigoQR ||
   //   !inputs.nombre_usuario || !inputs.photo_url)
   //   return res.json({ res: "error", detail: "Complete todos los campos" })
-  var photo_post = 'https://s3.us-east-2.amazonaws.com/gn8images/'+req.file.originalname;
+  var photo_post = 'https://s3.us-east-2.amazonaws.com/gn8images/' + req.file.originalname;
   var post = {
     nombre_post: inputs.nombre_post,
     photo_post: photo_post,
@@ -60,12 +60,12 @@ app.post('/ws/create_post', upload.single('picture'), function (req, res, next) 
     likesCount: 0,
     liked: {},
     commentsCount: 0,
-    codigoqr: inputs.codigoqr=='1'?true:false,
-    codigoqr_des:inputs.codigoqr_des,
+    codigoqr: inputs.codigoqr == '1' ? true : false,
+    codigoqr_des: inputs.codigoqr_des,
     createdAt: new Date(),
     updateAt: new Date()
   }
- 
+
   db.collection('posts').insert(post, (err, post) => {
     if (err) return res.json({ res: "error", detail: err })
     // enviar a todos los sockets que estan escuchando
@@ -75,26 +75,75 @@ app.post('/ws/create_post', upload.single('picture'), function (req, res, next) 
   });
 
 });
+app.post('/ws/generarCodigoQR', function (req, res) {
+  var inputs = req.body
+
+  var invitacion = {
+    id_post: inputs.id_post,
+    id_usuario_invitado: inputs.id_usuario_invitado,
+    nombre_post: inputs.nombre_post,
+    photo_post: inputs.photo_post,
+    codigoqr_des: inputs.codigoqr_des,
+    id_usuario: inputs.id_usuario,
+    nombre_usuario: inputs.nombre_usuario,
+    photo_url: inputs.photo_url,
+
+    createdAt: new Date(),
+    updateAt: new Date()
+  }
+
+  db.collection('invitaciones').insert(invitacion, (err, invitacion) => {
+    if (err) return res.json({ res: "error", detail: err })
+    return res.json({ res: "ok", invitacion })
+  });
+});
 app.post('/ws/posts/', function (req, res) {
   var posts = db.collection('posts')
     .find({})
+    .skip(10 * (req.body.page - 1)).limit(10)
     .sort({ createdAt: -1 })
     .toArray((err, posts) => {
       // If there aren't any posts, then return.
-      if (!posts.length) return res.json({ res: "error", post: [] });
+      if (err) return res.json({ res: "error", detail: err });
       res.json({ res: "ok", posts });
+    });
+})
+app.post('/ws/getCodigo_Usuario/', function (req, res) {
+  var posts = db.collection('invitaciones')
+    .find({
+      id_post: req.body.id_post,
+      id_usuario_invitado: req.body.id_usuario_invitado
+    })
+    .toArray((err, invitacion) => {
+      // If there aren't any posts, then return.
+      if (err) return res.json({ res: "error", detail: err });
+      res.json({ res: "ok", invitacion });
+    });
+})
+app.post('/ws/like_post/', function (req, res) {
+  //const id = mongojs.ObjectId(req.body.id_post)
+  var posts = db.collection('likes_posts')
+    .find({ id_post: req.body.id_post })
+    .limit(10)
+    .toArray((err, likes) => {
+      // If there aren't any posts, then return.
+      if (err) return res.json({ res: "error", detail: err });
+      res.json({ res: "ok", likes });
     });
 })
 /**
  * Comentarios de un post
  */
 app.post('/ws/comments', function (req, res) {
+  console.log(req.body.page)
   var messages = db.collection('message')
     .find({ id_post: req.body.id_post })
-    //.sort({ createdAt: 1 })
+    .sort({ createdAt: -1 })
+    .skip(25 * (req.body.page - 1)).limit(25)
     .toArray((err, messages) => {
+      //console.log(messages.length)
       // If there aren't any messages, then return.
-      if (!messages.length) return res.json({ res: "error", comments: [] });
+      if (err) return res.json({ res: "error", detail: err });
       res.json({ res: "ok", comments: messages });
     });
 
@@ -140,20 +189,47 @@ function _sendAndSaveMessage(message, socket, fromServer) {
 }
 function _guardarLikePost(like, socket) {
   const id = mongojs.ObjectId(like.id_post)
+  //const like = like
   const campo = "liked." + like.id_user
-  db.collection('posts').update(
-    { _id: id },
-    {
-      $set: { [campo]: like },
-      $inc: { likesCount: like.like ? 1 : -1 }
-    }, function (err, res) {
-      // the update is complete 
-      console.log(res)
-      if (err) console.log(err)
-      // If the message is from the server, then send to everyone.
-      var emitter = socket.broadcast;
-      emitter.emit('like_post', like);
-    })
+  db.collection('likes_posts')
+    .find({ id_post: like.id_post, id_user: like.id_user })
+    .toArray((err, liked) => {
+      if (err) return res.json({ res: "error", detail: err });
+      if (liked.length > 0) {
+        //Update like user
+
+        db.collection('likes_posts')
+          .update({ id_post: like.id_post, id_user: like.id_user },
+          { $set: { like: like.like } },
+          function (err, res) {
+            if (err) console.log(err)
+            db.collection('posts').update(
+              { _id: id },
+              {
+                $set: { [campo]: like.like },
+                $inc: { likesCount: like.like ? 1 : -1 }
+              }, function (err, res) {
+                // the update is complete 
+                if (err) console.log(err)
+                // If the message is from the server, then send to everyone.
+                websocket.emit('like_post', like);
+              })
+          })
+      } else {
+        //Insert new like
+        db.collection('likes_posts').insert(like, (err, liked) => {
+          db.collection('posts').update(
+            { _id: id }, {
+              $set: { [campo]: like.like },
+              $inc: { likesCount: 1 }
+            }, function (err, post) {
+              // the update is complete 
+              if (err) return console.log(err)
+              websocket.emit('like_post', like);
+            })
+        });
+      }
+    });
 }
 // Allow the server to participate in the chatroom through stdin.
 var stdin = process.openStdin();
